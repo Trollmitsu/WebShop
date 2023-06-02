88ab041c18d6e506d7ed73a2b7fff02b502473c3
Danish Gill <Danish.gill@hotmail.com>
2023-06-02 14:02:21 +0200

    latest

--- WebShop.WebApi/Data/ShopOnlineDbContext.cs
+++ WebShop.WebApi/Data/ShopOnlineDbContext.cs
@@ -22,7 +22,7 @@ namespace WebShop.WebApi.Data
                 Name = "Glossier - Beauty Kit",
                 Description = "A kit provided by Glossier, containing skin care, hair care and makeup products",
                 ImageURL = "/Images/Beauty/Beauty1.png",
-                Price = 100,
+                Price = 499,
                 Quantity = 100,
                 CategoryId = 1
 
@@ -33,7 +33,7 @@ namespace WebShop.WebApi.Data
                 Name = "Curology - Skin Care Kit",
                 Description = "A kit provided by Curology, containing skin care products",
                 ImageURL = "/Images/Beauty/Beauty2.png",
-                Price = 50,
+                Price = 249,
                 Quantity = 45,
                 CategoryId = 1
 
@@ -44,7 +44,7 @@ namespace WebShop.WebApi.Data
                 Name = "Cocooil - Organic Coconut Oil",
                 Description = "A kit provided by Curology, containing skin care products",
                 ImageURL = "/Images/Beauty/Beauty3.png",
-                Price = 20,
+                Price = 129,
                 Quantity = 30,
                 CategoryId = 1
 
@@ -55,7 +55,7 @@ namespace WebShop.WebApi.Data
                 Name = "Schwarzkopf - Hair Care and Skin Care Kit",
                 Description = "A kit provided by Schwarzkopf, containing skin care and hair care products",
                 ImageURL = "/Images/Beauty/Beauty4.png",
-                Price = 50,
+                Price = 2499,
                 Quantity = 60,
                 CategoryId = 1
 
@@ -66,7 +66,7 @@ namespace WebShop.WebApi.Data
                 Name = "Skin Care Kit",
                 Description = "Skin Care Kit, containing skin care and hair care products",
                 ImageURL = "/Images/Beauty/Beauty5.png",
-                Price = 30,
+                Price = 179,
                 Quantity = 85,
                 CategoryId = 1
 
@@ -78,7 +78,7 @@ namespace WebShop.WebApi.Data
                 Name = "Air Pods",
                 Description = "Air Pods - in-ear wireless headphones",
                 ImageURL = "/Images/Electronic/Electronics1.png",
-                Price = 100,
+                Price = 1199,
                 Quantity = 120,
                 CategoryId = 3
 
@@ -89,7 +89,7 @@ namespace WebShop.WebApi.Data
                 Name = "On-ear Golden Headphones",
                 Description = "On-ear Golden Headphones - these headphones are not wireless",
                 ImageURL = "/Images/Electronic/Electronics2.png",
-                Price = 40,
+                Price = 599,
                 Quantity = 200,
                 CategoryId = 3
 
@@ -100,7 +100,7 @@ namespace WebShop.WebApi.Data
                 Name = "On-ear Black Headphones",
                 Description = "On-ear Black Headphones - these headphones are not wireless",
                 ImageURL = "/Images/Electronic/Electronics3.png",
-                Price = 40,
+                Price = 400,
                 Quantity = 300,
                 CategoryId = 3
 
@@ -111,7 +111,7 @@ namespace WebShop.WebApi.Data
                 Name = "Sennheiser Digital Camera with Tripod",
                 Description = "Sennheiser Digital Camera - High quality digital camera provided by Sennheiser - includes tripod",
                 ImageURL = "/Images/Electronic/Electronic4.png",
-                Price = 600,
+                Price = 1399,
                 Quantity = 20,
                 CategoryId = 3
 
@@ -122,7 +122,7 @@ namespace WebShop.WebApi.Data
                 Name = "Canon Digital Camera",
                 Description = "Canon Digital Camera - High quality digital camera provided by Canon",
                 ImageURL = "/Images/Electronic/Electronic5.png",
-                Price = 500,
+                Price = 1299,
                 Quantity = 15,
                 CategoryId = 3
 
@@ -133,7 +133,7 @@ namespace WebShop.WebApi.Data
                 Name = "Nintendo Gameboy",
                 Description = "Gameboy - Provided by Nintendo",
                 ImageURL = "/Images/Electronic/technology6.png",
-                Price = 100,
+                Price = 799,
                 Quantity = 60,
                 CategoryId = 3
             });
@@ -144,7 +144,7 @@ namespace WebShop.WebApi.Data
                 Name = "Black Leather Office Chair",
                 Description = "Very comfortable black leather office chair",
                 ImageURL = "/Images/Furniture/Furniture1.png",
-                Price = 50,
+                Price = 499,
                 Quantity = 212,
                 CategoryId = 2
             });
@@ -155,7 +155,7 @@ namespace WebShop.WebApi.Data
                 Name = "Pink Leather Office Chair",
                 Description = "Very comfortable pink leather office chair",
                 ImageURL = "/Images/Furniture/Furniture2.png",
-                Price = 50,
+                Price = 449,
                 Quantity = 112,
                 CategoryId = 2
             });
@@ -165,7 +165,7 @@ namespace WebShop.WebApi.Data
                 Name = "Lounge Chair",
                 Description = "Very comfortable lounge chair",
                 ImageURL = "/Images/Furniture/Furniture3.png",
-                Price = 70,
+                Price = 699,
                 Quantity = 90,
                 CategoryId = 2
             });
@@ -175,7 +175,7 @@ namespace WebShop.WebApi.Data
                 Name = "Silver Lounge Chair",
                 Description = "Very comfortable Silver lounge chair",
                 ImageURL = "/Images/Furniture/Furniture4.png",
-                Price = 120,
+                Price = 1399,
                 Quantity = 95,
                 CategoryId = 2
             });
@@ -185,7 +185,7 @@ namespace WebShop.WebApi.Data
                 Name = "Porcelain Table Lamp",
                 Description = "White and blue Porcelain Table Lamp",
                 ImageURL = "/Images/Furniture/Furniture6.png",
-                Price = 15,
+                Price = 129,
                 Quantity = 100,
                 CategoryId = 2
             });
@@ -195,7 +195,7 @@ namespace WebShop.WebApi.Data
                 Name = "Office Table Lamp",
                 Description = "Office Table Lamp",
                 ImageURL = "/Images/Furniture/Furniture7.png",
-                Price = 20,
+                Price = 89,
                 Quantity = 73,
                 CategoryId = 2
             });
@@ -206,7 +206,7 @@ namespace WebShop.WebApi.Data
                 Name = "Puma Sneakers",
                 Description = "Comfortable Puma Sneakers in most sizes",
                 ImageURL = "/Images/Shoes/Shoes1.png",
-                Price = 100,
+                Price = 799,
                 Quantity = 50,
                 CategoryId = 4
             });
@@ -216,7 +216,7 @@ namespace WebShop.WebApi.Data
                 Name = "Colorful Trainers",
                 Description = "Colorful trainsers - available in most sizes",
                 ImageURL = "/Images/Shoes/Shoes2.png",
-                Price = 150,
+                Price = 299,
                 Quantity = 60,
                 CategoryId = 4
             });
@@ -226,7 +226,7 @@ namespace WebShop.WebApi.Data
                 Name = "Blue Nike Trainers",
                 Description = "Blue Nike Trainers - available in most sizes",
                 ImageURL = "/Images/Shoes/Shoes3.png",
-                Price = 200,
+                Price = 999,
                 Quantity = 70,
                 CategoryId = 4
             });
@@ -236,7 +236,7 @@ namespace WebShop.WebApi.Data
                 Name = "Colorful Hummel Trainers",
                 Description = "Colorful Hummel Trainers - available in most sizes",
                 ImageURL = "/Images/Shoes/Shoes4.png",
-                Price = 120,
+                Price = 499,
                 Quantity = 120,
                 CategoryId = 4
             });
@@ -246,7 +246,7 @@ namespace WebShop.WebApi.Data
                 Name = "Red Nike Trainers",
                 Description = "Red Nike Trainers - available in most sizes",
                 ImageURL = "/Images/Shoes/Shoes5.png",
-                Price = 200,
+                Price = 1299,
                 Quantity = 100,
                 CategoryId = 4
             });
@@ -256,7 +256,7 @@ namespace WebShop.WebApi.Data
                 Name = "Birkenstock Sandles",
                 Description = "Birkenstock Sandles - available in most sizes",
                 ImageURL = "/Images/Shoes/Shoes6.png",
-                Price = 50,
+                Price = 399,
                 Quantity = 150,
                 CategoryId = 4
             });
---
+++ WebShop.WebApi/Migrations/20230531211059_init5.cs
@@ -0,0 +1,340 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebShop.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class init5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Price",
+                value: 499m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Price",
+                value: 249m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Price",
+                value: 129m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Price",
+                value: 249m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Price",
+                value: 179m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Price",
+                value: 1199m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Price",
+                value: 599m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Price",
+                value: 400m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Price",
+                value: 1399m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Price",
+                value: 1299m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "Price",
+                value: 799m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "Price",
+                value: 499m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "Price",
+                value: 449m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "Price",
+                value: 699m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "Price",
+                value: 1399m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "Price",
+                value: 129m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "Price",
+                value: 89m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "Price",
+                value: 799m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "Price",
+                value: 299m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "Price",
+                value: 999m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 21,
+                column: "Price",
+                value: 499m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "Price",
+                value: 1299m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 23,
+                column: "Price",
+                value: 399m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Price",
+                value: 100m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Price",
+                value: 50m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Price",
+                value: 20m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Price",
+                value: 50m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Price",
+                value: 30m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Price",
+                value: 100m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Price",
+                value: 40m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Price",
+                value: 40m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Price",
+                value: 600m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Price",
+                value: 500m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "Price",
+                value: 100m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "Price",
+                value: 50m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "Price",
+                value: 50m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "Price",
+                value: 70m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "Price",
+                value: 120m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "Price",
+                value: 15m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "Price",
+                value: 20m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "Price",
+                value: 100m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "Price",
+                value: 150m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "Price",
+                value: 200m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 21,
+                column: "Price",
+                value: 120m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "Price",
+                value: 200m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 23,
+                column: "Price",
+                value: 50m);
+        }
+    }
+}
